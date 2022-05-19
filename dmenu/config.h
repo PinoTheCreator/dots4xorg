a3e41e73f2bb7ea644a09bf37a440a20a786730e
pinothegamer <101257700+pinothegamer@users.noreply.github.com>
2022-05-19 16:33:29 +0200

    Add files via upload

---
+++ dmenu/config.h
@@ -0,0 +1,40 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 1;                    /* -c option; centers dmenu on screen */
+static int min_width = 250;                    /* minimum width when centered */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+	"monospace:size=12"
+};
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+
+static const char highlight[] = "#ffff00";
+static const char prime[] = "#005577";
+
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#bbbbbb", "#222222" },
+	[SchemeSel] = { "#eeeeee", prime },
+	[SchemeSelHighlight] = { highlight, prime },
+	[SchemeNormHighlight] = { highlight, "#222222" },
+	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeOutHighlight] = { "#ffc978", "#00ffff" },
+};
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 10;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
+
+/*
+ * Use prefix matching by default; can be inverted with the -x flag.
+ */
+static int use_prefix = 1;
+
+/* Size of the window border */
+static const unsigned int border_width = 2;
