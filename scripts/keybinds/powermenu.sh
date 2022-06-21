33eb95167fd7a0de37fe62816241e61e1dbcc998
PinoTheCreator <101257700+PinoTheCreator@users.noreply.github.com>
2022-06-21 15:38:32 +0200

    Delete scripts directory

--- scripts/keybinds/powermenu.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-OPTIONS=$(echo -e "-Poweroff\n-Reboot" | dmenu)
-
-case $OPTIONS in
-	"-Poweroff") SUDO_ASKPASS=~/scripts/dpass.sh sudo -A poweroff;;
-	"-Reboot") SUDO_ASKPASS=~/scripts/dpass.sh sudo -A reboot;;
-	*) dunstify "Invalid entry!" && exit 1;;
-esac
